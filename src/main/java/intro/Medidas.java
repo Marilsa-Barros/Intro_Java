c6fb1bab64aeb4684763c7ae1178aff9ef765831
Marilsa-Barros <marilsa_barros@hotmail.com>
2021-06-17 13:33:48 -0300

    Criamos novos métodos de contarAtéDez e contagemRegressiva até de mostrar como utilizar a classe Scanner para ler o que o usuário escolheu no teclado

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -1,47 +1,1 @@
-// 1- Pacote
-package intro;
-
-// 2 - Referência as bibliotecas
-
-import java.sql.SQLOutput;
-
-// 3- Classe
-public class Medidas {
-    //3.1 Atributos - Caracteristicas
-
-    //3.2 - Métodos e Funções
-    public static void main(String[] args){
-    calcularAreaModoCurto();
-    calcularAreaModoExtenso();
-
-    }
-
-    public static void calcularAreaModoCurto(){
-        System.out.println("Cálculo de área Mood Curto");
-        // Calculo de área - Reduzido
-        int largura = 4;
-        int comprimento = 3;
-
-        System.out.println("Para a largura de " + largura + "m e o comprimento de "
-                +  comprimento + "m a area é de " + largura * comprimento + "m2");
-
-    }
-
-    public static void calcularAreaModoExtenso(){
-        System.out.println("Cálculo de área Mood Extenso");
-        // Calculo de área - Exemplo o tamnho do tapete ou do piso
-        int largura;
-        int comprimento;
-        int resultado;
-
-        largura = 5;
-        comprimento = 6;
-
-        resultado = largura * comprimento;
-        System.out.println("Para a largura de " + largura + "m e o comprimento de "
-                +  comprimento + "m a area é de " + resultado + "m2");
-
-    }
-
-
-}
+// 1- Pacotepackage intro;// 2 - Referência as bibliotecasimport java.sql.SQLOutput;import java.util.Scanner;// 3- Classepublic class Medidas {    //3.1 Atributos - Caracteristicas    //3.2 - Métodos e Funções    public static void main(String[] args){        // Condicional = Verificar umna condição- Fazer uma pergunta para uma pessoa, um hadware umou software        // Utilizar a classe Scanner do Java para ler a escolha do usuário no console        Scanner scanner= new Scanner(System.in);        System.out.println("MENU DE O P Ç Õ E S: ");        System.out.println("c -  Calcular Area Modo Curto");        System.out.println("d -  Contar até Dez");        System.out.println("e -  Calcular Area Modo Extenso");        System.out.println("i -  IF Simples");        System.out.println("r -  Contagem regressiva de 10 a 0");        System.out.println("Digite a opção desejada: ");        String opcao= scanner.next();        // Switch = selecionar o comportamento do programa conforme a escolha da pessoa ou do sofware        //String opcao = "curto";        switch (opcao){            case "c":            case "C":                System.out.println("Você escolheu executar o método calcularAreaModoCurto");                calcularAreaModoCurto();                break;            case "d":            case "D":                System.out.println("Você escolheu executar o método contarAteDez");                contarAteDez();                break;            case "e":            case "E":                System.out.println("Você escolheu executar o método calcularAreaModoExtenso");                calcularAreaModoExtenso();                break;            case "i":            case "I":                System.out.println("Você escolheu executar o método ifSimples");                ifSimples();                break;            case "r":            case "R":                System.out.println("Você escolheu executar o método contagemRegressiva");                contagemRegressiva();                break;            default:                System.out.println("Você escolheu outro valor que não tem uma ação associada");                break;        }    }    public static void ifSimples(){        // Condicional = Verificar umna condição- Fazer uma pergunta para uma pessoa, um hadware umou software        // if = se        // else = senão        String modo = "curto";        if (modo == "curto"){            calcularAreaModoCurto();        }        else {            calcularAreaModoExtenso();        }    }    public static void calcularAreaModoCurto(){        System.out.println("Cálculo de área Modo Curto");        // Calculo de área - Reduzido        int largura = 4;        int comprimento = 3;        System.out.println("Para a largura de " + largura + "m e o comprimento de "                +  comprimento + "m a area é de " + largura * comprimento + "m2");    }    public static void calcularAreaModoExtenso(){        System.out.println("Cálculo de área Modo Extenso");        // Calculo de área - Exemplo o tamnho do tapete ou do piso        int largura;        int comprimento;        int resultado;        largura = 5;        comprimento = 6;        resultado = largura * comprimento;        System.out.println("Para a largura de " + largura + "m e o comprimento de "                +  comprimento + "m a area é de " + resultado + "m2");    }    public static void contarAteDez(){        // Loops ou Repetições        // for = repetição incondicional        System.out.println("Contagem Crescente");        for (int numero = 1; numero <= 10; numero++){            System.out.println(numero);        }    }    public static void contagemRegressiva(){        System.out.println("Contagem Regressiva");        for (int numero = 10; numero > -1; numero--){            System.out.println(numero);        }    }}
